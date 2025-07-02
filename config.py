bd0164acfe3da8f69c09f2e106f11ff017f547e9
Muhammed Erbay <muhammederbay2003@gmail.com>
2025-07-02 09:06:53 +0300

    Add model save path configuration to the Config class

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     ZIP_FILENAME = "dog-breed-identification.zip"
     DATA_URL = "https://www.kaggle.com/c/dog-breed-identification/data"
     MODEL_URL  = "https://kaggle.com/models/google/mobilenet-v2/TensorFlow2/130-224-classification/1"
+    MODEL_SAVE_PATH = "saved_model/dog_breed_model.h5"
     IMG_SIZE = 224
     NUM_CLASSES = 120
     UNIQUE_BREEDS = None
